95bb6237876aa83242b413e5ae53341c9cabf7f6
Alexis Montagne <alexis.montagne@gmail.com>
2021-04-13 14:31:02 -0400

    stacktrace: Make PackageeName function public

--- reporter/sentry/reporter.go
+++ reporter/sentry/reporter.go
@@ -145,26 +145,8 @@ func extractStacktrace(err error, n int) *sentry.Stacktrace {
 }
 
 func splitQualifiedFunctionName(name string) (string, string) {
-	pkg := packageName(name)
+	pkg := stacktrace.PackageName(name)
 	return pkg, strings.TrimPrefix(name, pkg+".")
-}
-
-func packageName(name string) string {
-	// A prefix of "type." and "go." is a compiler-generated symbol that doesn't belong to any package.
-	// See variable reservedimports in cmd/compile/internal/gc/subr.go
-	if strings.HasPrefix(name, "go.") || strings.HasPrefix(name, "type.") {
-		return ""
-	}
-
-	pathend := strings.LastIndex(name, "/")
-	if pathend < 0 {
-		pathend = 0
-	}
-
-	if i := strings.Index(name[pathend:], "."); i != -1 {
-		return name[:pathend+i]
-	}
-	return ""
 }
 
 func isInApp(absPath, module string) bool {
