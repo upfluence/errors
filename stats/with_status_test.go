bff37e6f04e7a2db3f6e0ed17558736651dc2eac
Alexis Montagne <alexis.montagne@gmail.com>
2021-04-13 12:26:02 -0400

    stats: Test primitives

---
+++ stats/with_status_test.go
@@ -0,0 +1,50 @@
+package stats_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/upfluence/errors"
+	"github.com/upfluence/errors/errtest"
+	"github.com/upfluence/errors/stats"
+	"github.com/upfluence/errors/tags"
+)
+
+func TestWithStauts(t *testing.T) {
+	errtest.TestErrorWrapper(
+		t,
+		func(err error) error {
+			return errors.WithStatus(err, "foo")
+		},
+		errtest.ErrorWrapperOptions{N: 1},
+	)
+}
+
+func TestGetStatus(t *testing.T) {
+	assert.Equal(
+		t,
+		"success",
+		stats.GetStatus(nil),
+	)
+
+	assert.Equal(
+		t,
+		"baz",
+		stats.GetStatus(errors.New("baz")),
+	)
+
+	assert.Equal(
+		t,
+		"foo",
+		stats.GetStatus(errors.WithStatus(errors.New("bar"), "foo")),
+	)
+}
+
+func TestGetTags(t *testing.T) {
+	assert.Equal(
+		t,
+		map[string]interface{}{"domain": "github.com/upfluence/errors/stats", "status": "baz"},
+		tags.GetTags(stats.WithStatus(errors.New("bar"), "baz")),
+	)
+}
