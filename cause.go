21ec1be20f4ae46f48ad1b5489ff47bf2d51d62c
Alexis Montagne <alexis.montagne@gmail.com>
2023-04-03 14:48:36 -0700

    .: Add IsTimeout helper

--- cause.go
+++ cause.go
@@ -11,3 +11,17 @@ func Unwrap(err error) error { return base.UnwrapOnce(err) }
 
 func As(err error, target interface{}) bool { return errors.As(err, target) }
 func Is(err, target error) bool             { return errors.Is(err, target) }
+
+type timeout interface {
+	Timeout() bool
+}
+
+func IsTimeout(err error) bool {
+	var terr timeout
+
+	if As(err, &terr) && terr.Timeout() {
+		return true
+	}
+
+	return false
+}
