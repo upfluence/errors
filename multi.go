1d683215ca89eefee0935cea255578a72efade73
Xavier Petit <xavier.petit@upfluence.com>
2023-10-23 10:22:18 +0200

    multi: Add Join (alias Combine)

--- multi.go
+++ multi.go
@@ -4,3 +4,5 @@ import "github.com/upfluence/errors/multi"
 
 func WrapErrors(errs []error) error { return WithFrame(multi.Wrap(errs), 1) }
 func Combine(errs ...error) error   { return WithFrame(multi.Wrap(errs), 1) }
+
+var Join = Combine
