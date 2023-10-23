732b65f81e020e3ff1f473dd51526685c4b5362c
Xavier Petit <xavier.petit@upfluence.com>
2023-10-23 16:41:25 +0200

    multi: Use a function instead of a variable

--- multi.go
+++ multi.go
@@ -4,5 +4,4 @@ import "github.com/upfluence/errors/multi"
 
 func WrapErrors(errs []error) error { return WithFrame(multi.Wrap(errs), 1) }
 func Combine(errs ...error) error   { return WithFrame(multi.Wrap(errs), 1) }
-
-var Join = Combine
+func Join(errs ...error) error      { return WithFrame(multi.Wrap(errs), 1) }
